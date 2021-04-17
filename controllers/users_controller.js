440f7e182f4e7e842176f08b8a9247d0bc56f0dc
abhinavjeph <abhinavmeenameena@gmail.com>
2021-04-18 04:33:45 +0530

    add sign up form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user.js");
+
 module.exports.profile = function (req, res) {
   return res.render("user_profile", {
     title: "getSocial | Profile",
@@ -19,7 +21,31 @@ module.exports.signIn = function (req, res) {
 };
 
 module.exports.create = function (req, res) {
-  // TODO later
+  if (req.body.password != req.body.confirmPassword) {
+    console.log(req.body.password + " and " + req.body.confirmPassword);
+    console.log("Password did not match");
+    return res.redirect("back");
+  }
+
+  User.findOne({ emial: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("Error in finding the user in signing up");
+      return;
+    }
+
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("Error in creating the user");
+          return;
+        }
+        console.log("User created");
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
 };
 
 module.exports.createSession = function (req, res) {
