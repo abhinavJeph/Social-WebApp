32ac1fabbd0bde507309bba466dd6c63b548b883
abhinavjeph <abhinavmeenameena@gmail.com>
2021-04-23 01:04:46 +0530

    add feature of deleting post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 module.exports.create = function (req, res) {
   Post.create(
@@ -14,4 +15,24 @@ module.exports.create = function (req, res) {
       return res.redirect("back");
     }
   );
+};
+
+module.exports.destroy = function (req, res) {
+  Post.findById(req.params.id, function (err, post) {
+    if (err) {
+      console.log("Error in finding Post" + err);
+      return res.redirect("back");
+    }
+    // .id means converting the object _id into string
+    if (post.user == req.user.id) {
+      post.remove();
+      Comment.deleteMany({ post: req.params.id }, function (err) {
+        if (err) {
+          console.log("Error in deleting comments" + err);
+          return res.redirect("back");
+        }
+        return res.redirect("back");
+      });
+    }
+  });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const passport = require("passport");
 
 router.post("/create", passport.checkAuthentication, postsController.create);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  postsController.destroy
+);
 
 module.exports = router;
