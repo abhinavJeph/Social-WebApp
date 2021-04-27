8376980ec56a0bcb6ef243f8a0836da239d52d50
abhinavjeph <abhinavmeenameena@gmail.com>
2021-04-27 07:31:24 +0530

    create flash messages and show on layout

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 const MongoStore = require("connect-mongo")(session);
 const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMiddleWare = require("./config/middleware");
 
 app.use(
   sassMiddleware({
@@ -66,6 +68,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+// For Flash messages (Stored in session cookies)
+app.use(flash());
+
+// To pass on req.flash to the res.locals
+app.use(customMiddleWare.setFlash);
 
 // use express router
 app.use("/", require("./routes/index"));
