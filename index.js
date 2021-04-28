ebdc75da82f6de33dc50343a7a2f97bbaadb3f64
abhinavjeph <abhinavmeenameena@gmail.com>
2021-04-28 10:48:41 +0530

    show image on profile and handle edge case of multiple uploads

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static("./assets"));
+// make the upload path available to the browser
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // This should be before routes
 app.use(expressEjsLayouts);
