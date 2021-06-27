da6a915f5e4f3796114777a1f6c7dccb8740f1c0
abhinavjeph <abhinavmeenameena@gmail.com>
2021-06-27 21:44:40 +0530

    add like schema in DB

--- models/comment.js
+++ models/comment.js
@@ -14,6 +14,7 @@ const commentSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Like" }],
   },
   {
     timestamps: true,
--- models/post.js
+++ models/post.js
@@ -11,6 +11,7 @@ const postSchema = new mongoose.Schema(
       ref: "User",
     },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Like" }],
   },
   {
     timestamps: true,
